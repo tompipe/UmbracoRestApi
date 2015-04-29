dcadfe325f4f1feaec674bb5e5b3a5a44e53e046
Shannon <sdeminick@gmail.com>
2015-04-29 15:11:37 +1000

    last commit before removing the collection+json and Odata stuff

--- src/Umbraco.Web.Rest/Models/HAL/ContentRepresentation.cs
+++ src/Umbraco.Web.Rest/Models/HAL/ContentRepresentation.cs
@@ -11,33 +11,6 @@ using WebApi.Hal;
 
 namespace Umbraco.Web.Rest.Models.HAL
 {
-    public static class LinkTemplates
-    {
-        public static class Content
-        {
-            public static Link RootContent
-            {
-                get { return new Link("root", "~/content"); }
-            }
-
-            public static Link ContentItem
-            {
-                get { return new Link("content", "~/content/{id}"); }
-            }
-
-            public static Link ParentContent
-            {
-                get { return new Link("parent", "~/content/{parentId}"); }
-            }
-
-            public static Link ChildContent
-            {
-                get { return new Link("children", "~/content/{id}/children"); }
-            }
-        }
-
-    }
-
     public class ContentRepresentation : Representation
     {
         public ContentRepresentation()
---
+++ src/Umbraco.Web.Rest/Models/HAL/LinkTemplates.cs
@@ -0,0 +1,31 @@
+﻿using WebApi.Hal;
+
+namespace Umbraco.Web.Rest.Models.HAL
+{
+    public static class LinkTemplates
+    {
+        public static class Content
+        {
+            public static Link RootContent
+            {
+                get { return new Link("root", "~/content"); }
+            }
+
+            public static Link ContentItem
+            {
+                get { return new Link("content", "~/content/{id}"); }
+            }
+
+            public static Link ParentContent
+            {
+                get { return new Link("parent", "~/content/{parentId}"); }
+            }
+
+            public static Link ChildContent
+            {
+                get { return new Link("children", "~/content/{id}/children"); }
+            }
+        }
+
+    }
+}
